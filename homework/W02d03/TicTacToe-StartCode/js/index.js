555fec9ad42ff907ccf0942ee226af0272b4e876
Tech-A44 <gmafutala07@gmail.com>
2017-08-10 01:35:19 +0100

    layout ofpp

--- homework/W02d03/TicTacToe-StartCode/js/index.js
+++ homework/W02d03/TicTacToe-StartCode/js/index.js
@@ -5,14 +5,17 @@ var player1 = [];
 var player2 = [];
 // find clear button
 var clearBoard = $("#reset")
-// find the number of turns
+// who's turn
 var whosTurn;
+// array of the winning statements 
+var winningCombinations =[[0,1,2],[3,4,5],[6,7,8],[1,4,7],[0,4,8],[2,5,8],[0,3,6],[2,4,6]];
+
 
 // functions for the jQuery to work
 $(function (event) {
 	// function for the each player to make a move
 	$(boxes).each(function(index, box){
-	  console.log($(box).attr("data-num")); // show the data numbers on the console
+	  // console.log($(box).attr("data-num")); // show the data numbers on the console
 	  	// assign the player1 moves to the grid
 	  	$(box).on("click", function() {
 	  		if (whosTurn = true) {
@@ -20,19 +23,27 @@ $(function (event) {
 	  			$(box).addClass("X").html("X");
 	  			whosTurn = false;
 	  			console.log(player1);
+	  		} 
+	  		// assign the player2 moves to the grid
+	  		else if (whosTurn = null) {
+	  			console.log("you here")
+	  			player2.push($(box).attr("data-num"));
+	  			$(box).addClass("O").html("O");
+	  			whosTurn = true;
+	  			console.log(player2);
 	  		}
-		
 		}) 
 	})
-	
-
-	// assign the player1 moves to the grid
-	
-	// assign the player2 moves to the grid
 
 	// functions for win combination
-		// array of the winning statements 
+	function compare(compValue,players){
+	
+	for(var i=0;i<winningCombinations.length;i++){
+		logic(compValue,i,player);
 
+	}
+}
+	function compare(player1Combination,player1Score);
 
 	// function to clear the board
 
