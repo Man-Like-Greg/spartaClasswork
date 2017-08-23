3cb3aa23ef5988e3700118452935102d2be1a204
Tech-A44 <gmafutala07@gmail.com>
2017-08-23 18:37:19 +0100

    index running correctly

---
+++ homework/W04d03/IMDB/app.rb
@@ -0,0 +1,9 @@
+# I will be creating a rate movie review 
+
+# where the user can add in a movie name
+
+# the user can choose there rating 
+
+# the user can add there review 
+
+# the user can add a link to the trailer 
--- homework/W04d03/IMDB/controllers/movies_controller.rb
+++ homework/W04d03/IMDB/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < Sinatra::Base
 
+	#setting the routes as the parent dirctory for the current file
+	set :root, File.join(File.dirname(__FILE__), "..")
+
+	#setting the view directory 
+	set :view, Proc.new{ File.join(root, "views") }
+
+	configure :development do 
+		register Sinatra::Reloader
+	end
 
 
 
